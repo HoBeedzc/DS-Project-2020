4a61012c03ff386ba11821700cb20442c8c7be6b
Hobee <lzqpublic@163.com>
2020-04-05 09:41:06 +0800

    sloved chose-to-do questions

--- 6_1_Eight_Queens.c
+++ 6_1_Eight_Queens.c
@@ -6,3 +6,65 @@ More info: https://github.com/HoBeedzc/ds_project
 ©copyright 1999-2020 Hobee. All rights reserved.
 Never Settle
 */
+
+// 本程序基于以下假设实现Eight Queens问题
+// 1.请确保您的输入使问题有意义
+
+/*
+这个问题的解决方案我先在这里写一下吧，不然一会就忘了...
+首先要明确的是肯定每行只有一个皇后，所以可以使用一个长度为8的列表，列表的每一项代表第i行的皇后所在的位置。
+然后进行回溯，并输出。
+*/
+# include<stdio.h>
+# include<stdlib.h>
+# include<stdbool.h>
+
+int Eight_queens(int cur);
+int show(int *pos);
+
+int main()
+{
+    Eight_queens(0);
+    return 0;
+}
+
+int Eight_queens(int cur)
+{
+    static int pos[8] = {0};
+    if (cur == 8)
+    {
+        show(pos);
+        return 0;
+    }
+    for (int i = 0; i < 8;i++)
+    {
+        bool flag = true;
+        pos[cur] = i;
+        for (int j = 0; j < cur;j++)
+        {
+            if(pos[j]==pos[cur]||abs(pos[j]-pos[cur])==abs(cur-j))
+            {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) Eight_queens(cur + 1);
+    }
+}
+
+int show(int *pos)
+{
+    static int cnt = 1;
+    printf("No.%d\n", cnt++);
+    for (int i = 0; i < 8;i++)
+    {
+        for(int j = 0; j < 8;j++)
+        {
+            if (j == pos[i]) printf("Q ");
+            else printf("* ");
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
--- 6_2_Knight_Tour.c
+++ 6_2_Knight_Tour.c
@@ -6,3 +6,69 @@ More info: https://github.com/HoBeedzc/ds_project
 ©copyright 1999-2020 Hobee. All rights reserved.
 Never Settle
 */
+
+// 本程序基于以下假设实现Knight Tour问题
+// 1.请确保您的输入使问题有意义
+// 2.n不要大于20
+
+/*
+由于老师没有给出具体题目，所以我再网上找了一个骑士旅行问题。
+在一个N*N 格子的棋盘上，有一只国际象棋的骑士在棋盘的左下角，骑士只能根据象棋的规则进行移动，要么横向跳动一格纵向跳动两格，要么纵向跳动一格横向跳动两格。骑士从第一个格子出发，每个格子只能访问一次，能否访问完所有的格子， 请找到一个解决方案。
+*/
+
+# include<stdio.h>
+# include<stdlib.h>
+
+# define MAXSIZE 20
+
+int Knight_tour(int n, int x, int y, int cnt);
+int show(int map[MAXSIZE][MAXSIZE], int n);
+
+int main()
+{
+    int n;
+    printf("Enter the value of n...\n");
+    scanf("%d", &n);
+    if (Knight_tour(n, 0, 0, 1) == -1) printf("No such answer!\n");
+    return 0;
+}
+
+int Knight_tour(int n, int x, int y, int cnt)
+{
+    static int X[8] = {2, 1, -1, -2, -2, -1, 1, 2};
+    static int Y[8] = {1, 2, 2, 1, -1, -2, -2, -1};
+    static int map[MAXSIZE][MAXSIZE] = {0};
+    int nextx, nexty;
+    map[x][y] = cnt;
+    if (cnt == n*n)
+    {
+        show(map, n);
+        return 0;
+    }
+    for (int i = 0; i < 8;i++)
+    {
+        nextx = x + X[i];
+        nexty = y + Y[i];
+        if (nextx>=0&&nextx<n&&nexty>=0&&nexty<n&&(map[nextx][nexty]<=0||map[nextx][nexty]>=cnt))
+        {
+            if(Knight_tour(n, nextx, nexty, cnt + 1) == 0) return 0;
+        }
+    }
+    map[x][y] = -1;
+    //show(map, n);
+    return -1;
+}
+
+int show(int map[MAXSIZE][MAXSIZE], int n)
+{
+    for (int i = 0; i < n;i++)
+    {
+        for (int j = 0; j < n;j++)
+        {
+            printf("%3d", map[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    return 0;
+}
