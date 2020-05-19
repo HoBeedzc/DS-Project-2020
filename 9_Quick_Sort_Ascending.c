f0a480a9778db287e49983adc1bd497699d8352c
Hobee <lzqpublic@163.com>
2020-05-19 20:51:29 +0800

    add some questions

---
+++ 9_Quick_Sort_Ascending.c
@@ -0,0 +1,14 @@
+/*
+Author: Hobee (LZQ)
+Date: 2019.5.19
+Problem: 09 Quick Sort with Ascending Order
+More info: https://github.com/HoBeedzc/ds_project
+Website: http://blog.hobeedzc.cn/
+©copyright 1999-2020 Hobee. All rights reserved.
+Never Settle
+*/
+
+// 本程序基于以下假设实现Quick Sort问题
+// 1.请确保您的输入使问题有意义
+// 2.请使用-1表示输入结束
+// 3.输入的节点个数请不要超过9999.
