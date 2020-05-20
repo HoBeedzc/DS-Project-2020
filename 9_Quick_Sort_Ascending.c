42dfeb9bc86c4eb79cc726a3d01caf3aba550b35
Hobee <lzqpublic@163.com>
2020-05-20 13:12:36 +0800

    optimize ninth question

--- 9_Quick_Sort_Ascending.c
+++ 9_Quick_Sort_Ascending.c
@@ -16,17 +16,31 @@ Never Settle
 # include<stdio.h>
 
 int Every_step_exchenge(int *A, int m, int n);
+int Continuous_exchange(int *A, int m, int n);
+int Left_right_exchange(int *A, int m, int n);
 int Show(int *A);
 
 int main()
 {
-    int A[9999] = {0};
+    int A[9999] = {0}, B[9999] = {0}, C[9999] = {0};
     printf("Enter the length of list...\n");
     scanf("%d",&A[0]);
     printf("Enter the value of every node, splited by space...\n");
     for (int i = 1; i <= A[0];i++) scanf("%d", &A[i]);
+    for (int i = 0; i <= A[0];i++) B[i] = A[i];
+    for (int i = 0; i <= A[0];i++) C[i] = A[i];
     Every_step_exchenge(A, 1, A[0]);
+    Continuous_exchange(B, 1, B[0]);
+    Left_right_exchange(C, 1, C[0]);
+    printf("Using every step exchenge...\n");
     Show(A);
+    printf("\n");
+    printf("Using continuous exchenge...\n");
+    Show(B);
+    printf("\n");
+    printf("Using left right exchenge...\n");
+    Show(C);
+    printf("\n");
     return 0;
 }
 
@@ -36,37 +50,75 @@ int Every_step_exchenge(int *A, int m, int n) //每步交换法
     if (i>=j) return 0;
     while(1)
     {
-        while(A[i] <= A[j]) j--;
-        if (i>=j)
+        while(i<j && A[i] <= A[j]) j--;
+        if (i<j)
         {
-            Every_step_exchenge(A, m, i - 1);
-            Every_step_exchenge(A, i + 1, n);
-            return 0;
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
         }
-        temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
-        while (A[i] <= A[j]) i++;
-        if (i>=j)
+        while (i<j && A[i] <= A[j]) i++;
+        if (i<j)
+        {
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+        if(i==j)
         {
             Every_step_exchenge(A, m, j - 1);
             Every_step_exchenge(A, j + 1, n);
             return 0;
         }
-        temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
     }
 }
 
-int Continuous_exchange(int *A) //持续交换法
+int Continuous_exchange(int *A, int m, int n) //持续交换法
 {
-    return 0;
+    int i = m, j = n, temp = A[m];
+    if (i>=j) return 0;
+    while(1)
+    {
+        while(i<j && temp <= A[j]) j--;
+        if (i<j) A[i] = A[j];
+        while (i<j && A[i] <= temp) i++;
+        if (i<j) A[j] = A[i];
+        if (i==j)
+        {
+            A[i] = temp;
+            Continuous_exchange(A, m, j - 1);
+            Continuous_exchange(A, j + 1, n);
+            return 0;
+        }
+    }
 }
 
-int Left_right_exchange(int *A) //左右交换法
+int Left_right_exchange(int *A, int m, int n) //左右交换法
 {
-    return 0;
+    int i = m + 1, j = n, temp;
+    if (i>j) return 0;
+    while(1)
+    {
+        while(i<j && A[m] <= A[j]) j--;
+        while (i<j && A[i] <= A[m]) i++;
+        if (i<j)
+        {
+            temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+        if (i==j)
+        {
+            if (n == m + 1 && A[m] < A[n]) i = m;
+            else if (A[m] < A[i]) i--;
+            temp = A[m];
+            A[m] = A[i];
+            A[i] = temp;
+            Left_right_exchange(A, m, i - 1);
+            Left_right_exchange(A, i + 1, n);
+            return 0;
+        }
+    }
 }
 
 int Show(int *A)
